312be51e5bbd752e78b7ea6f207fa123ee8fa1f5
sanjay <sanjay@10.19.88.203>
2017-05-10 12:46:16 -0700

    Hittable and level updated

---
+++ src/Hittable.java
@@ -0,0 +1,10 @@
+
+public class Hittable extends Actor {
+
+	@Override
+	public void act(long now) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/Level.java
+++ src/Level.java
@@ -1,4 +1,15 @@
+import java.util.ArrayList;
 
 public abstract class Level extends World {
+	
+	private ArrayList<Hittable> target= new ArrayList<Hittable>();
+	private ArrayList<Hittable> civilian= new ArrayList<Hittable>();
+	private int numBullet= 10; //Change every level
+	
+	
+	int numTargets=target.size();
+	int numCivilians=civilian.size();
+	
+	
 
 }
