50e923f5538104f59345676637131601cccd10da
xgeng043 <xgeng043@CHS-312-35.chs.win.fuhsd.org>
2017-05-11 09:35:23 -0700

    Added a few dummy methods to Level class

--- src/Level.java
+++ src/Level.java
@@ -2,19 +2,39 @@ import java.util.ArrayList;
 
 public abstract class Level extends World {
 	
-	private ArrayList<Hittable> target;
-	private ArrayList<Hittable> civilian;
+	private ArrayList<Hittable> targets;
+	private ArrayList<Hittable> civilians;
 	private int numMaxBullets;
 	
 	public Level() {
 		// Use the "super" keyword in subclass constructors to invoke this.
-		target = new ArrayList<Hittable>();
-		civilian = new ArrayList<Hittable>();
+		super();
+		targets = new ArrayList<Hittable>();
+		civilians = new ArrayList<Hittable>();
 		numMaxBullets = 10; // Default value
+	}
+	
+	@Override
+	public void act(long now) {
+		if (isLost()) {
+			
+		}
 	}
 	
 	public void setNumMaxBullets(int n) {
 		numMaxBullets = n;
 	}
+	
+	private boolean isWon() {
+		return (targets.size() == 0);
+	}
+	
+	private boolean isLost() {
+		return false;
+	}
+	
+	protected abstract String displayLostMessage();
+	
+	protected abstract String displayWonMessage();
 
 }
