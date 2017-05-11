d24113e3fa1c4fe656192189257341c8a624fe5c
xgeng043 <xgeng043@CHS-312-35.chs.win.fuhsd.org>
2017-05-11 09:36:11 -0700

    added a few dummy methods to Level

---
+++ levels/LevelTesting.java
@@ -0,0 +1,23 @@
+
+public class LevelTesting extends Level {
+	
+	public static void main(String args[]) {
+		Level lt = new LevelTesting();
+	}
+	
+	public LevelTesting() {
+		super();
+	}
+
+	@Override
+	protected String displayLostMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String displayWonMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
