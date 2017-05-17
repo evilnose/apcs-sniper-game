c990497d9fc71a7224b3b87cdda2cd1116defeca
Ishu <Ishu@10.231.115.103>
2017-05-16 18:13:06 -0700

    Made some changes to level 1

--- levels/Level1.java
+++ levels/Level1.java
@@ -6,7 +6,7 @@ public class Level1 extends Level
 	{
 		super(numLevel);
 		
-		for(int i = 0;i<10;i++)
+		for(int i = 0;i<5;i++)
 		{
 			int xPos = (int) (Math.random()*SniperGame.LEVEL_WIDTH);
 			int yPos = 350;
@@ -16,18 +16,6 @@ public class Level1 extends Level
 			t.setPos(xPos, yPos);
 			
 		}
-//		Tester tester1 = new Tester(false);
-//		addHittable(tester1);
-//		tester1.setPos(200, 350);
-//		
-//		Tester tester2= new Tester(true);
-//		addHittable(tester2);
-//		tester2.setPos(450, 350);
-//		
-//		Tester tester3=new Tester(false);
-//		addHittable(tester3);
-//		tester3.setPos(550, 350);
-//		
 		Scope scope = new Scope(150);
 		addScope(scope);
 		setOnMouseTracking(scope);
