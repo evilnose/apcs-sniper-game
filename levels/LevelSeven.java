840590b5b67a0f80408114e055d85023248a2f59
aditi <sanjay@10.0.0.134>
2017-05-30 17:33:54 -0700

    level 7 tester

--- levels/LevelSeven.java
+++ levels/LevelSeven.java
@@ -1,48 +1,53 @@
 import javafx.scene.image.Image;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
 
 public class LevelSeven extends Level 
 {
 	
 	public LevelSeven(int numLevel) 
 	{
-		super(numLevel);		
-		//levelMessage = "AN ALIEN SUSPECT IS SEEN IN THE MOVIE THEATER. PROTECT THE MOVIEGOERS AND ELIMINATE THE ALIEN"
-		//+ " THE SUSPECT BEFORE IT ESCAPES THE SCENE.";	
-	    levelMessage=".";
-		BackgroundImage myBI = new BackgroundImage(new Image("file:sprites/backgrounds/level_7.jpeg"), BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
-				BackgroundSize.DEFAULT);
-		this.setBackground(new Background(myBI));
+		super(numLevel);
+		levelMessage = "A GROUP OF ALIENS IS WREAKING HAVOC IN THIS MALL. AS YOU WILL SEE, THEY DO NOT QUITE UNDERSTAND THE CONCEPT OF SOCIAL ETIQUETTE. TRANQUILIZE THEM BEFORE THEY DRIVE EVERYONE CRAZY!";	
+
 	}
 	
 	@Override
 	protected void addAllHittables()
 	{
 
-		Sitter sitter = new Sitter(true, 0.4);
-		this.addHittable(sitter);
-		sitter.setPos(400, 100);
-		
-		Runner r1 = new Runner(false,1);
-		r1.setPos(100, 10);
-		this.addHittable(r1);
-		
+
+		Sleeper sl1 = new Sleeper(true, 0.5);
+		this.addHittable(sl1);
+		sl1.setPos(240, 350);
+
+		StreetRunner sr1 = new StreetRunner(false, 0.4,-0.1,-1);
+		this.addHittable(sr1);
+		sr1.setPos(700, 90);
+		sr1.setBounds(600,1000);
 	
+		StreetRunner sr2 = new StreetRunner(false, 0.35,2.5,-1);
+		this.addHittable(sr2);
+		sr2.setPos(-7,270);
 		
+		Jumper s1 = new Jumper(true, 0.3,500);
+		this.addHittable(s1);
+		s1.setPos(45, 55);
+		
+		
+		StreetWalker sw1 = new StreetWalker(false,0.4,0.6,1);
+		this.addHittable(sw1);
+	    sw1.setPos(350,260);
+		sw1.setBounds(600,1000);
 	}
 	
 	@Override
 	protected String getDescription() {
-		return "This is level seven";
+		return "Hey rookie, let's make your first mission easy. You see that alien sitting alone at the bus stop? Eliminate it"+
+				" before it gets away.";
 	}
 
 	@Override
 	protected String getName() {
-		return "Level 7";
+		return "Tutorial";
 	}
 
 }
